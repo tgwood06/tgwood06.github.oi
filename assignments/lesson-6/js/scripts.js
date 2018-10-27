d153d3e3b0828ec0346eca8511200310974042cb
tgwood06 <tgwood06@byui.edu>
2018-10-27 10:20:30 -0700

    commit change

--- assignments/lesson-6/js/scripts.js
+++ assignments/lesson-6/js/scripts.js
@@ -4,16 +4,4 @@
 
 function toggleMenu(){
     /*console.log(document.getElementById("horizontalnav").classList);*/
-    document.getElementById("horizontalnav").classList.toggle("hide");
-}
-
-
-var d = new Date();
-var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var wholedate = day[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
-
-document.getElementById("date").innerHTML = wholedate;
-/*var d = new Date();
-document.getElementById("demo").innerHTML = d;
- var date = new Date(toDateString)???*/
+    document.getElementById("primaryNav").classList.toggle("hide");}
