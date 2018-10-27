02e96addb1ec4ac959185795c69f31c0d891216e
tgwood06 <tgwood06@byui.edu>
2018-10-27 10:07:30 -0700

    commit change

--- assignments/lesson-6/js/scripts.js
+++ assignments/lesson-6/js/scripts.js
@@ -1,3 +1,13 @@
+/*function toggleMenu() {
+    console.log("it worked");
+}*/
+
+function toggleMenu(){
+    /*console.log(document.getElementById("horizontalnav").classList);*/
+    document.getElementById("horizontalnav").classList.toggle("hide");
+}
+
+
 var d = new Date();
 var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
