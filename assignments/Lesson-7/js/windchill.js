483c9024e09007f3a6605c1f5a2a57d70cb8e4aa
tgwood06 <tgwood06@byui.edu>
2018-11-01 11:56:55 -0700

    commit changes

--- assignments/Lesson-7/js/windchill.js
+++ assignments/Lesson-7/js/windchill.js
@@ -5,7 +5,9 @@ the air temperature in Fahrenheit, and s is the wind speed in miles per hour.*/
 
 
 
-var t= 56;
-var s= 5;
-var f = 35.74 + 0.6215*t - 35.75 * Math.pow(s,0.16) + 0.4275 * t *Math.pow(s,0.16);
-document.getElementById("windchill").innerHTML = Math.round(f);
+    var t = 56;
+    var s = 5;
+    var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    document.getElementById("windchill").innerHTML = Math.round(f);
+
+
